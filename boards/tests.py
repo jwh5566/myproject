a2e51db064c178150ed9821a939e4bdb6ed41568
jwh5566 <jwh5566@foxmail.com>
2017-09-27 07:50:47 +0800

    add templates

--- boards/tests.py
+++ boards/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.urls import resolve
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 
 class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         """
         test home page return code 200
@@ -21,3 +27,52 @@ class HomeTests(TestCase):
         """
         view = resolve('/')
         self.assertEqual(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        """
+        test home page have link to the board_topics
+        :return:
+        """
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        """
+         test board_topics return success status code
+        :return:
+        """
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        """
+        test when the board is not in the database
+        :return:
+        """
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        """
+        test board_topics mapping the view
+        :return:
+        """
+        view = resolve('/boards/1/')
+        self.assertEqual(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_home_page(self):
+        """
+        test board_topics have link to the home page
+        :return:
+        """
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
--- boards/views.py
+++ boards/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Board
 
 
 def home(request):
-    # return HttpResponse('Hello World!')
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
